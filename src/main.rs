28e970d1da34dd4afeb2caeee2a01751419d8754
Andreas Resch <andreas@resch.io>
2020-04-01 22:15:33 +0200

    added Dockerfile and gitlab-ci

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,22 @@
+use chrono::{Utc};
 use serde::Deserialize;
 use std::io::Write;
 use std::fs::OpenOptions;
 use std::{thread, time};
 
 #[derive(Debug, Deserialize)]
+struct Log {
+    date: String,
+    data: ApiResult
+}
+
+#[derive(Debug, Deserialize, Clone)]
 struct Song {
     artist: String,
     title: String
 }
 
-#[derive(Debug, Deserialize)]
+#[derive(Debug, Deserialize, Clone)]
 struct ApiResult {
     past: Song,
     present: Option<Song>,
@@ -17,29 +24,35 @@ struct ApiResult {
 }
 
 fn main() {
-    let mut result = String::from("");
+    let mut result = md5::compute(String::from("not a valid song"));
     loop {
         let re = get_json("http://www.suedtirol1.it/routing/acc_fun_interaktiv/api/v1/playlist/index.json?v=1").unwrap();
-        if re != result {
+        let past = md5::compute(&re.past.title);
+        if past != result {
             let mut file = OpenOptions::new()
                 .write(true)
                 .append(true)
                 .create(true)
                 .open("/var/tmp/suedtirol1")
                 .unwrap();
+            let past = md5::compute(&re.past.title);
+            let log = Log {
+                date : Utc::now().to_string(),
+                data : re,
+            };
 
-            if let Err(e) = writeln!(file, "{}", re) {
+            if let Err(e) = writeln!(file, "{:?}", log) {
                 eprintln!("Couldn't write to file: {}", e);
             }
-            result = re;
+            result = past;
         }
         thread::sleep(time::Duration::from_millis(1000 * 60));
     }
 }
 
-fn get_json(url: &str) -> Result<String, Box <dyn std::error::Error>> {
+fn get_json(url: &str) -> Result<ApiResult, Box <dyn std::error::Error>> {
     let body = reqwest::blocking::get(url)?.text()?;
     let res : ApiResult = serde_json::from_str(&body).expect("JSON was not well-formatted");
-    println!("{:?}", res);
-    Ok(body)
+    //println!("{:?}", res);
+    Ok(res)
 }
