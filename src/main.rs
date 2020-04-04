af468f4e820a23fe48dfe6263776db6e213eabe4
Andreas Resch <andreas@resch.io>
2020-04-04 22:29:22 +0200

    fixed crashing on invalid json and improved logging

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,6 @@
 use chrono::Utc;
+use rand::Rng;
+use regex::Regex;
 use serde::{Deserialize, Serialize};
 use std::fs::OpenOptions;
 use std::io::Write;
@@ -7,52 +9,88 @@ use std::{thread, time};
 #[derive(Debug, Deserialize, Serialize)]
 struct Log {
     date: String,
-    data: ApiResult,
+    data: Option<Song>,
 }
 
 #[derive(Debug, Deserialize, Serialize, Clone)]
 struct Song {
     artist: String,
     title: String,
+    is_new: Option<bool>,
 }
 
 #[derive(Debug, Deserialize, Serialize, Clone)]
 struct ApiResult {
-    past: Song,
+    past: Option<Song>,
     present: Option<Song>,
-    future: Song,
+    future: Option<Song>,
 }
 
 fn main() {
+    let regex = Regex::new(r"\*NEU\*").unwrap();
     let mut result = String::from("");
     loop {
         let re = get_json(
             "http://www.suedtirol1.it/routing/acc_fun_interaktiv/api/v1/playlist/index.json?v=1",
-        )
-        .unwrap();
-        if re.past.title != result {
-            let mut file = OpenOptions::new()
-                .write(true)
-                .append(true)
-                .create(true)
-                .open("/var/tmp/suedtirol1")
-                .unwrap();
-            result = re.past.title.clone();
-            let log = Log {
-                date: Utc::now().to_string(),
-                data: re,
-            };
+        );
+        match re {
+            Ok(mut re) => {
+                let future = re.clone().future.unwrap().title;
+                if re.past.is_none() && re.present.is_none() {
+                    re.present = re.future.clone();
+                }
+                if future != result {
+                    let mut copy = re.present.clone().unwrap();
 
-            if let Err(e) = writeln!(file, "{}", serde_json::to_string(&log).unwrap()) {
-                eprintln!("Couldn't write to file: {}", e);
+                    copy.is_new = Some(if regex.is_match(&copy.title) {
+                        copy.title.split_off(copy.title.len() - 1 - 4);
+                        true
+                    } else {
+                        false
+                    });
+
+                    let mut file = OpenOptions::new()
+                        .write(true)
+                        .append(true)
+                        .create(true)
+                        .open("/var/tmp/suedtirol1")
+                        .unwrap();
+                    result = future.clone();
+                    let log = Log {
+                        date: Utc::now().to_string(),
+                        data: Some(copy),
+                    };
+                    if let Err(e) = writeln!(file, "{}", serde_json::to_string(&log).unwrap()) {
+                        eprintln!("Couldn't write to file: {}", e);
+                    }
+                }
             }
+            Err(_) => {}
         }
-        thread::sleep(time::Duration::from_millis(1000 * 60));
+        thread::sleep(time::Duration::from_millis(
+            60000 + rand::thread_rng().gen_range(0, 30000),
+        ));
     }
 }
 
 fn get_json(url: &str) -> Result<ApiResult, Box<dyn std::error::Error>> {
-    let body = reqwest::blocking::get(url)?.text()?;
-    let res: ApiResult = serde_json::from_str(&body).expect("JSON was not well-formatted");
-    Ok(res)
+    let response = reqwest::blocking::get(url);
+
+    match response {
+        Ok(response) => {
+            let body = response.text();
+            match body {
+                Ok(b) => {
+                    let result = serde_json::from_str(&b);
+
+                    match result {
+                        Ok(result) => return Ok(result),
+                        Err(e) => return Err(Box::new(e)),
+                    }
+                }
+                Err(e) => return Err(Box::new(e)),
+            }
+        }
+        Err(e) => return Err(Box::new(e)),
+    }
 }
